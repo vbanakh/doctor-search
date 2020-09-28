70c1c2d575fc5b8332c98f1016ef43af13c36e7e
vbanakh <mykush.vasylyna@gmail.com>
2020-09-28 13:43:33 +0300

    preloader config

---
+++ src/app/preloader-config.ts
@@ -0,0 +1,8 @@
+import { NgxUiLoaderConfig } from 'ngx-ui-loader';
+export const loaderConfig: NgxUiLoaderConfig = {
+    fgsType: 'chasing-dots',
+    fgsColor: '#6dc3f5',
+    overlayColor:  "rgba(150, 150, 150, 0.8)",
+    pbThickness: 2,
+    pbColor: '#6dc3f5',
+}
