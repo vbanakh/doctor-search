f7c8b4073ec543b619957941fbe47587b0ae19a8
vbanakh <mykush.vasylyna@gmail.com>
2020-10-02 14:10:02 +0300

    update gitignore with environmens

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyB1w2MBc4deZtU-pKBEbufnuFu1KDBGaNU",
+    authDomain: "doctorsearch-b3561.firebaseapp.com",
+    databaseURL: "https://doctorsearch-b3561.firebaseio.com",
+    projectId: "doctorsearch-b3561",
+    storageBucket: "doctorsearch-b3561.appspot.com",
+    messagingSenderId: "192780542438",
+    appId: "1:192780542438:web:3d7728be310e5299087627"
+  }
 };
